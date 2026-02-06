be36be2a6469e7b0b5cc32509282457116dda915
nine-pixel431 <nainamishra1222@gmail.com>
2026-02-06 06:44:34 +0530

    Add files via upload

---
+++ 1.1 Check if a number is even or odd.py
@@ -0,0 +1,6 @@
+print("Greetings User!")
+no=int(input("Enter a number to check is it is even or odd: "))
+if no%2==0:
+    print(f"Your number, {no}, is even")
+else:
+    print(f"Your number, {no}, is odd")
